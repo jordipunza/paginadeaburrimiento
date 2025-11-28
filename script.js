c0e99aa8a62afbe3aa64ca3e21b00fcee248fdac
jordipunza <jorditetaspunzantes@gmail.com>
2025-11-28 22:40:48 +0100

    a

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ let musica = new Audio("img/musicauno/fondo.mp3");
 musica.loop = true;
 
 window.addEventListener("load", () => {
-  // El intro permanece visible hasta que el usuario haga clic
+
   const intro = document.getElementById("intro");
 
   document.addEventListener("click", () => {
-    // Ocultamos el intro
+
     if (intro) intro.style.display = "none";
 
-    // Reproducimos la música
+
     musica.play().catch(() => {
       console.log("Autoplay bloqueado, pero ya se intentó iniciar.");
     });
-  }, { once: true }); // solo la primera vez
+  }, { once: true });
 });
 
 function toggleMusica() {
